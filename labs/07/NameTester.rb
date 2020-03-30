ada3eb8b958c999187549f1c46eefad621663a7f
Jacob@UW <jacobpeterbrink@gmail.com>
2020-03-30 13:55:13 -0500

    finished lab 7

--- labs/07/NameTester.rb
+++ labs/07/NameTester.rb
@@ -1,27 +1,46 @@
 # NameTester.rb tests class Name and its operations
 #
 # Begun by: Dr. Adams, for CS 214 at Calvin College.
-# Completed by:
-# Date:
+# Completed by: Jacob Brink
+# Date: 3/30/2020
+# Lab: 7
 ####################################################
 
 require 'test/unit/assertions'   # needed for assert
 include Test::Unit::Assertions
 
-# Replace this line with the declaration of class Name
+class Name
+
+  def initialize(first, middle, last)
+    @first, @middle, @last = first, middle, last
+  end
+
+  attr_reader :first, :middle, :last
+
+  def fullName()
+    @first + " " + @middle + " " + @last
+  end
+
+  def print()
+    puts self.fullName
+    self.fullName
+  end
+  
+end
 
 
 def testName
-#   name = Name.new("John", "Paul", "Jones")
+   name = Name.new("John", "Paul", "Jones")
 
-#   assert name.first == "John", "first failed"
-#   assert name.middle == "Paul", "middle failed"
-#   assert name.last == "Jones", "last failed"
-#   assert name.fullName == "John Paul Jones", "fullName failed"
-#   assert name.print == "John Paul Jones", "print failed"
+   assert name.first == "John", "first failed"
+   assert name.middle == "Paul", "middle failed"
+   assert name.last == "Jones", "last failed"
+   assert name.fullName == "John Paul Jones", "fullName failed"
+   assert name.print == "John Paul Jones", "print failed"
    
    print "All tests passed!\n"
 end
 
-testName
-
+if __FILE__ == $0
+  testName()
+end
